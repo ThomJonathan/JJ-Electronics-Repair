a080ebccd862f4a39b4150f346b1e4304e588e24
Jonathan Thom <thom44160@gmail.com>
2026-09-09 21:39:02 +0200

    fixed typescript errors

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 const categories = [
